6a9fdea8336f88f3acf67bf66e2591efd38601fe
Ronald <kyoberonald@gmail.com>
2020-01-23 03:00:16 +0000

    poster-shop
    poster-shop

---
+++ public/script.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: "#app",
+    data: {
+        total:0,
+        products: [
+            {title: "Product 1", id: 1, price: 9.99 },
+            {title: "Product 2", id: 2, price: 9.99 },
+            {title: "Product 3", id: 3, price: 9.99 }   
+             ],
+    cart:[]
+    },
+    methods: {
+        addToCart:function(product){
+          this.total += product.price;
+          var found = false;
+          for (var i = 0; i <this.cart.length; i++){
+              if (this.cart[i].id === product.id) {
+                  this.cart[i].qty++;
+                  found = true;
+              }
+          }
+          if (!found) {
+            this.cart.push({
+                id: product.id,
+                title: product.title,
+                price: product.price,
+                qty: 1
+            }); 
+
+          }
+
+        }
+    }
+});
